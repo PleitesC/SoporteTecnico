8562645c01e5a214a71877b9e6f77db6b20b1527
mcappalleti <josselinemari@hotmail.es>
2019-08-23 04:22:54 -0500

    agg arquitectura

--- SoporteT/SoporteT/Controllers/ProductosController.cs
+++ SoporteT/SoporteT/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using SoporteT.Models;
+﻿using Productos.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +13,8 @@ namespace SoporteT.Controllers
         public ActionResult Index()
         {
 
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Mouse";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Teclado";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 1;
-            producto3.Descripcion = "Pantalla";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
 
             return View(listadeProductos);
