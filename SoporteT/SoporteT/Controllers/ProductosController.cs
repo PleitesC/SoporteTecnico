8c84b89c88f5f0e360c07d10ea3ec49e8ec55d66
mcappalleti <josselinemari@hotmail.es>
2019-08-23 02:55:54 -0500

    agregando una lista de productos

---
+++ SoporteT/SoporteT/Controllers/ProductosController.cs
@@ -0,0 +1,38 @@
+﻿using SoporteT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoporteT.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Mouse";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Teclado";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 1;
+            producto3.Descripcion = "Pantalla";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+
+            return View(listadeProductos);
+        }
+
+    }
+}
